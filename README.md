f90b09ca3f7f245e44377b1ad6fbd16b10458ea9
kevin.yauy <kevin.yauy@seqone.fr>
2021-01-08 11:55:47 +0100

    fix(compare_variant): :bug: Select only minor major warning change type variant to get smaller and more useful dataframe Solve memory issue

--- README.md
+++ README.md
@@ -1,22 +1,66 @@
-# Genome Alert! App
+# Genome Alert! 
 
 ![Genome Alert! logo](www/genome_alert_title.png)
 
-## Overview 
+## Overview
 
-This repository contains the Genome Alert! shiny app.  
+This repository contains the Genome Alert! framework and shiny app.  
 
 Genome Alert! is an automatic tracking and self-curating method of variant pathogenicity and genotype-phenotype knowledge in ClinVar database.
 
 This app display processed ClinVar data through the Genome Alert! framework : 
-- **ClinVCF** monitors submissions and extracts clinical information from a Clinvar XML monthly Full Release into an easy-to-manipulate standard VCF 4.2 file.    
+- **ClinVCF** monitors submissions and extracts clinical information from a Clinvar XML monthly Full Release into an easy-to-manipulate standard VCF 4.2 file.  
 In order to reclassify high confidence pathogenic variants with a conflicting interpretation of pathogenicity status, ClinVCF first removes ACMG classification outliers submissions for variants with at least 4 submissions according to the 1.5 * Interquartile Range method. Secondly, it reclassifies the variant status according to the ClinVar classification system and finally we set the reclassification confidence with a 3-stars level system.  [https://github.com/SeqOne/clinvcf](https://github.com/SeqOne/clinvcf)   
 - **Variant Alert!** track every significant changes in variant classification and gene-disease association between two versions of ClinVCF VCFs, including their “breaking change” status defined according to the suspected clinical impact of these changes. A major break change is defined as a potential direct impact on clinical diagnosis. A minor break change is a change in confidence classification (e.g. Pathogenic to Likely Pathogenic status). [https://github.com/SeqOne/variant_alert](https://github.com/SeqOne/variant_alert)  
 - **ClinVarome** gathers and processes all ClinVar knowledge available through time. Through an unsupervised clustering model, it classifies clinical validity of information for each gene with a 3-stars level system [https://gitlab.seq.one/labo/clinvarome](https://gitlab.seq.one/labo/clinvarome).  
 
-## Installation
+## Complete pipeline to generate the overall data
 
-**Requirements** 
+### Generate all VCFs from ClinVar data with ClinVCF
+
+First, install [ClinVCF](https://github.com/SeqOne/clinvcf) and then download all Clinvar XML file
+
+```bash
+cd OUTPUT_PATH/
+wget -r ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/
+```
+
+Copy all file into one folder 
+```bash
+mkdir OUTPUT_PATH/data
+find OUTPUT_PATH/ftp.ncbi.nlm.nih.gov/ -type f -print0 | xargs -0 cp -t OUTPUT_PATH/data/
+```
+
+Get list to process clinvcf (XML to VCF) 
+
+```bash
+ls OUTPUT_PATH/data/ | grep ".xml.gz" | grep -v "md5" > OUTPUT_PATH/data/clinvar_xml_list.txt
+```
+
+Process all ClinVar XML release with ClinVCF 
+```bash
+while read -r line ; do 
+  echo line
+  echo ${line}
+  echo filename
+  filename=$(echo "$line" | cut -d. -f1 | cut -d_ -f2) 
+  echo ${filename}
+  if [ -f "OUTPUT_PATH/data/clinvar_GRCh38_${filename}.vcf.gz" ]
+  then
+    echo "${filename} already processed"
+  else
+  clinvcf --genome GRCh38 --coding-first --gff data/GRCh38_latest_genomic.gff OUTPUT_PATH/data/${line} | bgzip -c > OUTPUT_PATH/data/clinvar_GRCh38_${filename}.vcf.gz
+  tabix -p vcf OUTPUT_PATH/data/clinvar_GRCh38_${filename}.vcf.gz
+  fi
+done < OUTPUT_PATH/data/clinvar_xml_list.txt
+```
+
+### 
+
+
+## Shiny app installation
+
+**Requirements**
 
 ```bash
 ubuntu-server
@@ -35,7 +79,7 @@ library(markdown)
 
 NB: Installation guide is available in RStudio [website](https://rstudio.com/products/shiny/download-server/ubuntu/).
 
-## Mandatory files
+### Mandatory files
 
 In addition to this repository, you will need to add 3 output files from Genome Alert!'s [ClinVarome](https://gitlab.seq.one/labo/clinvarome) in the `www/` folder :
 - compare-variant_total.tsv
--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 #
-# Shiny App for Variant Alert! exploration
+# Shiny App for Genome Alert! exploration
 #
 library(tidyverse)
 library(lubridate)
@@ -12,14 +12,13 @@ library(markdown)
 clinvarome <- read_delim("www/clinvar_GRCh38_2020-11_clinvarome_annotation.tsv",delim="\t",col_names = TRUE)
 clinvar_gene <- read_delim("www/compare-gene_total.tsv", delim="\t", col_names = TRUE)
 clinvar_variant <- read_delim("www/compare-variant_total.tsv", delim="\t", col_types = cols(clinvar_id = col_character()), col_names = TRUE)
-# col_types = cols(reclassification_status = col_integer())
-# Pre processing clinvar files
-#clinvar_variant$date <- gsub("\\.vcf\\.gz", "", clinvar_variant$name_clinvar_new )
-#clinvar_variant$date  <- gsub("clinvar_", "", clinvar_variant$name_clinvar_new )
+
+# Clean inputs 
 clinvar_variant$date  <- ymd(clinvar_variant$name_clinvar_new )
 clinvar_variant <- clinvar_variant %>%
     select(gene_info, variant_id, clinvar_id, old_classification, new_classification, confidence, breaking_change, reclassification_status, date) %>%
     rename(gene_symbol = gene_info, change_type = breaking_change) %>%
+    filter(!(change_type == ".." | change_type == "unknown")) %>%
     arrange(desc(date))
 
 clinvar_gene$date <- ymd(clinvar_gene$name_clinvar_new )
@@ -63,31 +62,31 @@ ui <-
                     )    
            ),
            
-            tabPanel("Compare variants",
-            
-                # Application title
-                titlePanel("Compare variants"),
-                fluidRow(
+           tabPanel("ClinVar-ome",
                     
-                    column(4,
-                           dateRangeInput("dates_cv", h3("Date range"),start="2017-10-16",min="2017-05-16")),
-                    column(8,
-                           plotOutput("plot_compare_variants")
-                    )
-                ),
-                
-                # print data table
-                DT::dataTableOutput('table_compare_variants'),
-                # add download button
-                downloadButton('downloadData_compare_variants', 'Download Variant Alert list'),
-                
-                # add logos
-                tags$div(
+                    # Application title
+                    titlePanel("ClinVar mendelian genome"),
+                    
+                    fluidRow(
+                        column(6,
+                               plotOutput("plot_star_clinvarome")),
+                        column(6,
+                               plotOutput("plot_date_clinvarome")
+                        )
+                        
+                    ),
+                    
+                    # print data table
+                    DT::dataTableOutput('table_clinvarome'),
+                    # add download button
+                    downloadButton('downloadData_clinvarome', 'Download ClinVarome list'),
+                    # add logos
+                    tags$div(
                         p(tags$a(img(src='logo-seqone.png', align = "right",width="200"), href="https://seq.one/")),
                         p(tags$a(img(src='logo-CHU.png', align = "right", width="100"), href="https://www.chu-rouen.fr/service/service-de-genetique/")),
                         p(tags$a(img(src='logo-uga.png', align = "right", width="90"), href="https://iab.univ-grenoble-alpes.fr/?language=en"))              
-                    )    
-                         ),
+                    )
+           ),
             
             tabPanel("Compare genes",
                      
@@ -97,7 +96,7 @@ ui <-
                 fluidRow(
                     
                     column(4,
-                           dateRangeInput("dates_cg", h3("Date range"),start="2017-10-16",min="2017-05-16")),
+                           dateRangeInput("dates_cg", h3("Date range"),start=(Sys.Date()-730),min="2017-05-16")),
                     column(8,
                            plotOutput("plot_compare_genes")
                            ),
@@ -115,79 +114,80 @@ ui <-
                 )
                 ),
             
-                
-            tabPanel("ClinVar-ome",
-                
-                # Application title
-                titlePanel("ClinVar mendelian genome"),
-                
-                fluidRow(
-                    column(6,
-                           plotOutput("plot_star_clinvarome")),
-                    column(6,
-                           plotOutput("plot_date_clinvarome")
-                     )
+           tabPanel("Compare variants",
                     
-                ),
-                
-                # print data table
-                DT::dataTableOutput('table_clinvarome'),
-                # add download button
-                downloadButton('downloadData_clinvarome', 'Download ClinVarome list'),
-                # add logos
-                tags$div(
-                    p(tags$a(img(src='logo-seqone.png', align = "right",width="200"), href="https://seq.one/")),
-                    p(tags$a(img(src='logo-CHU.png', align = "right", width="100"), href="https://www.chu-rouen.fr/service/service-de-genetique/")),
-                    p(tags$a(img(src='logo-uga.png', align = "right", width="90"), href="https://iab.univ-grenoble-alpes.fr/?language=en"))              
-            )
-            )
+                    # Application title
+                    titlePanel("Compare variants"),
+                    fluidRow(
+                        
+                        column(4,
+                               dateRangeInput("dates_cv", h3("Date range"),start=(Sys.Date()-730),min="2017-05-16")),
+                        column(8,
+                               plotOutput("plot_compare_variants")
+                        )
+                    ),
+                    
+                    # print data table
+                    DT::dataTableOutput('table_compare_variants'),
+                    # add download button
+                    downloadButton('downloadData_compare_variants', 'Download Variant Alert list'),
+                    
+                    # add logos
+                    tags$div(
+                        p(tags$a(img(src='logo-seqone.png', align = "right",width="200"), href="https://seq.one/")),
+                        p(tags$a(img(src='logo-CHU.png', align = "right", width="100"), href="https://www.chu-rouen.fr/service/service-de-genetique/")),
+                        p(tags$a(img(src='logo-uga.png', align = "right", width="90"), href="https://iab.univ-grenoble-alpes.fr/?language=en"))              
+                    )    
+           )               
+
     
         )
 
 # Define server logic required
 server <- function(input, output) {
-        
-    # process compare variant
+    # process Clinvarome
     
-    compare_variant_new <- reactive({
-            filter(clinvar_variant, between(date, as.Date(input$dates_cv[1]),as.Date(input$dates_cv[2])))
+    clinvarome_plot <- reactive({
+        clinvarome %>%
+            group_by(cluster_name) %>%
+            count()
     })
     
-    compare_variant_new_plot <- reactive({
-        compare_variant_new() %>%
-            filter(change_type != "..") %>%
-            group_by(month=floor_date(date, "2 month"), change_type) %>%
-            count() %>%
-            mutate(year= year(month))
-    })
+    output$plot_date_clinvarome <- renderPlot({
+        ggplot(clinvarome) + aes(x = first_path_var_date) + 
+            ggtitle("Gene's first (likely) pathogenic entry date distribution in ClinVarome") +
+            geom_bar()   
+    }
+    )
     
-    output$plot_compare_variants <- renderPlot({
-        ggplot(data = compare_variant_new_plot(), aes(x = month, y= n,  fill=change_type )) + 
-            ggtitle("Monitoring of change types in variant classification in ClinVar") +
+    output$plot_star_clinvarome <- renderPlot({
+        ggplot(clinvarome_plot(), aes(x="" , y=n, fill=cluster_name)) + 
+            ggtitle("Clinical validity of genes distribution in ClinVarome") +
             geom_bar(stat='identity') +
-            theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-            scale_x_date(date_labels = "%m-%Y",date_breaks = "2 month") + 
-            geom_text(aes(label=ifelse(change_type=="major",n,"")),color="black", position = position_stack(vjust = 0.5), size= 5)
-    })
+            coord_polar("y", start=0) +
+            theme_void()
+    }
+    )    
     
-    output$downloadData_compare_variants <- downloadHandler(
-        filename = function() { 
-            paste("variant-alert_", input$dates[1],"_",input$dates[2], ".tsv", sep="")
-        },
-        content = function(file) {
-            write_delim(compare_variant_new(), file ,delim = "\t", col_names = TRUE)
-        }, contentType = "text/tsv")
-    
-    output$table_compare_variants <- DT::renderDataTable(compare_variant_new(), filter = "top", options = list(pageLength = 15, scrollX = T, columnDefs = list(list(
-        targets = c(2,4,5,6),
+    output$table_clinvarome <- DT::renderDataTable(clinvarome, filter = "top", options = list(pageLength = 15,  scrollX = T, columnDefs = list(list(
+        targets = c(3,4,11,12),
         render = JS(
             "function(data, type, row, meta) {",
-            "return type === 'display' && data.length > 30 ?",
-            "'<span title=\"' + data + '\">' + data.substr(0, 30) + '...</span>' : data;",
+            "return type === 'display' && data.length > 40 ?",
+            "'<span title=\"' + data + '\">' + data.substr(0, 40) + '...</span>' : data;",
             "}")
     ))), callback = JS('table.page(3).draw(false);' )
     )
-
+    
+    output$downloadData_clinvarome <- downloadHandler(
+        filename = function() { 
+            #paste("ClinVarome_", Sys.Date(), ".tsv", sep="")
+            paste("ClinVarome_", clinvarome$clinvarome_date[1], ".tsv", sep="")
+        },
+        content = function(file) {
+            write_delim(clinvarome, file,delim = "\t", col_names = TRUE)
+        }, contentType = "text/tsv")
+    
         
     # process compare gene
     
@@ -229,48 +229,49 @@ server <- function(input, output) {
     
     output$table_compare_genes <- DT::renderDataTable(compare_gene_new(), filter = "top", options = list(pageLength = 15, scrollX = T))
     
-    # process Clinvarome
+    # process compare variant
     
-    clinvarome_plot <- reactive({
-        clinvarome %>%
-            group_by(cluster_name) %>%
-            count()
+    compare_variant_new <- reactive({
+        filter(clinvar_variant, between(date, as.Date(input$dates_cv[1]),as.Date(input$dates_cv[2])))
     })
     
-    output$plot_date_clinvarome <- renderPlot({
-        ggplot(clinvarome) + aes(x = first_path_var_date) + 
-            ggtitle("Gene's first (likely) pathogenic entry date distribution in ClinVarome") +
-            geom_bar()   
-    }
-    )
-
-    output$plot_star_clinvarome <- renderPlot({
-        ggplot(clinvarome_plot(), aes(x="" , y=n, fill=cluster_name)) + 
-            ggtitle("Clinical validity of genes distribution in ClinVarome") +
+    compare_variant_new_plot <- reactive({
+        compare_variant_new() %>%
+            filter(change_type != "..") %>%
+            group_by(month=floor_date(date, "2 month"), change_type) %>%
+            count() %>%
+            mutate(year= year(month))
+    })
+    
+    output$plot_compare_variants <- renderPlot({
+        ggplot(data = compare_variant_new_plot(), aes(x = month, y= n,  fill=change_type )) + 
+            ggtitle("Monitoring of change types in variant classification in ClinVar") +
             geom_bar(stat='identity') +
-            coord_polar("y", start=0) +
-            theme_void()
-        }
-    )    
-        
-    output$table_clinvarome <- DT::renderDataTable(clinvarome, filter = "top", options = list(pageLength = 15,  scrollX = T, columnDefs = list(list(
-        targets = c(3,4,11,12),
+            theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+            scale_x_date(date_labels = "%m-%Y",date_breaks = "2 month") + 
+            geom_text(aes(label=ifelse(change_type=="major",n,"")),color="black", position = position_stack(vjust = 0.5), size= 5)
+    })
+    
+    output$downloadData_compare_variants <- downloadHandler(
+        filename = function() { 
+            paste("variant-alert_", input$dates[1],"_",input$dates[2], ".tsv", sep="")
+        },
+        content = function(file) {
+            write_delim(compare_variant_new(), file ,delim = "\t", col_names = TRUE)
+        }, contentType = "text/tsv")
+    
+    output$table_compare_variants <- DT::renderDataTable(compare_variant_new(), filter = "top", options = list(pageLength = 15, scrollX = T, columnDefs = list(list(
+        targets = c(2,4,5,6),
         render = JS(
             "function(data, type, row, meta) {",
-            "return type === 'display' && data.length > 40 ?",
-            "'<span title=\"' + data + '\">' + data.substr(0, 40) + '...</span>' : data;",
+            "return type === 'display' && data.length > 30 ?",
+            "'<span title=\"' + data + '\">' + data.substr(0, 30) + '...</span>' : data;",
             "}")
     ))), callback = JS('table.page(3).draw(false);' )
     )
     
-    output$downloadData_clinvarome <- downloadHandler(
-        filename = function() { 
-            #paste("ClinVarome_", Sys.Date(), ".tsv", sep="")
-            paste("ClinVarome_", clinvarome$clinvarome_date[1], ".tsv", sep="")
-        },
-        content = function(file) {
-            write_delim(clinvarome, file,delim = "\t", col_names = TRUE)
-        }, contentType = "text/tsv")
+    
+    
 }
 
 # Run the application 
